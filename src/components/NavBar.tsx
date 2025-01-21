85fd4e0a26d533132b3a562162919ddd61d964a3
BatuhanGQskt <e231529@gmail.com>
2025-01-22 00:34:56 +0100

    Personal Logo added

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,10 +2,13 @@ import { blue } from '@mui/material/colors';
 import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 
+import PersonalLogo from '../assests/BG_logo.svg';
+
 function NavBar() {
   return (
     <Navbar expand="lg" sticky='top' className="bg-body-tertiary">
       <Container>
+        <img src={PersonalLogo} alt="personal-logo" style={{ width:'48px', height: '48px' }} />
         <Navbar.Brand style={{color:'white'}} href="#">My Portfolio</Navbar.Brand>
         <Navbar.Toggle id="toggle-navbar" aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav" >
